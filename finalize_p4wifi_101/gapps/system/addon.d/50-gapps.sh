819948dd6678489474cf69df597597c4ec255f06
Kasper Hettinga <kasper.hettinga@gmail.com>
2012-12-29 22:51:31 +0100

    Update p4wifi build scripts & gapps

---
+++ finalize_p4wifi_101/gapps/system/addon.d/50-gapps.sh
@@ -0,0 +1,80 @@
+#!/sbin/sh
+# 
+# /system/addon.d/50-gapps.sh
+#
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+app/CalendarGoogle.apk app/Calendar.apk
+app/ChromeBookmarksSyncAdapter.apk
+app/ConfigUpdater.apk
+app/GalleryGoogle.apk app/Gallery2.apk
+app/Gmail2.apk
+app/GmsCore.apk
+app/GoogleBackupTransport.apk
+app/GoogleContactsSyncAdapter.apk
+app/GoogleEars.apk
+app/GoogleFeedback.apk
+app/GoogleLoginService.apk
+app/GooglePartnerSetup.apk
+app/GoogleServicesFramework.apk
+app/GoogleTTS.apk
+app/LatinImeDictionaryPack.apk
+app/MediaUploader.apk
+app/Music2.apk app/Music.apk
+app/NetworkLocation.apk
+app/OneTimeInitializer.apk
+app/Phonesky.apk
+app/SetupWizard.apk app/Provision.apk
+app/Talk.apk
+app/Velvet.apk app/QuickSearchBox.apk
+app/VoiceSearchStub.apk
+etc/permissions/com.google.android.maps.xml
+etc/permissions/com.google.android.media.effects.xml
+etc/permissions/com.google.widevine.software.drm.xml
+etc/permissions/features.xml
+framework/com.google.android.maps.jar
+framework/com.google.android.media.effects.jar
+framework/com.google.widevine.software.drm.jar
+lib/libfilterpack_facedetect.so
+lib/libfrsdk.so
+lib/libgoogle_recognizer_jni.so
+lib/libgtalk_jni.so
+lib/liblightcycle.so
+lib/libpatts_engine_jni_api.so
+lib/libspeexwrapper.so
+lib/libvideochat_stabilize.so
+lib/libvorbisencoder.so
+media/LMprec_508.emd
+media/PFFprec_600.emd
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/$FILE
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/$FILE $R
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
